4509ed6d98dbe3ca1e5b185e9e11151a7c84db0c
Eliyahu Saks <emsaks@gmail.com>
2022-02-18 10:35:04 +0000

    commit

--- kno_partscan.c
+++ kno_partscan.c
@@ -26,68 +26,62 @@ static char func_name[NAME_MAX] = "device_add_disk";
 
 static int enabled = 1;
 module_param(enabled, int, 0664);
-MODULE_PARM_DESC(enabled, "Enable intercepting disk initializing so we can block partscan.");
 
 struct instance_data {
-	struct gendisk *disk;
+    struct gendisk *disk;
 };
 
 static int entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
-	struct instance_data *data;
-	struct gendisk *disk;
+    struct instance_data *data;
+    struct gendisk *disk;
 
-	data = (struct instance_data *)ri->data;
-	disk = (struct gendisk *)(regs->ARG);
+    data = (struct instance_data *)ri->data;
+    disk = (struct gendisk *)(regs->ARG);
 
-	if (!enabled || disk->flags & GENHD_FL_NO_PART_SCAN) {
-		data->disk = NULL;
-	} else {
-		pr_warn("Intercepted partition read for disk: %s.\n", disk->disk_name);
-		disk->flags |= (GENHD_FL_NO_PART_SCAN);
-		data->disk = disk; // store this so we can remove the NO_PARTSCAN flag on function return
-	}
-	return 0;
+    if (!enabled || disk->flags & GENHD_FL_NO_PART_SCAN) {
+        data->disk = NULL;
+    } else {
+        pr_warn("Intercepted partition read for disk: %s.\n", disk->disk_name);
+        disk->flags |= (GENHD_FL_NO_PART_SCAN);
+        data->disk = disk; // store this so we can remove the NO_PARTSCAN flag on function return
+    }
+    return 0;
 }
 
 static int ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
-	struct instance_data *data;
+    struct gendisk *disk;
 
-	data = (struct instance_data *)ri->data;
-	if (data->disk)
-		data->disk->flags &= ~(GENHD_FL_NO_PART_SCAN);
+    disk = ((struct instance_data *)ri->data)->disk;
+    if (disk) disk->flags &= ~(GENHD_FL_NO_PART_SCAN);
 
-	return 0;
+    return 0;
 }
 
 static struct kretprobe my_kretprobe = {
-	.handler		= ret_handler,
-	.entry_handler	= entry_handler,
-	.data_size		= sizeof(struct instance_data),
-	.maxactive		= 20,
+    .handler        = ret_handler,
+    .entry_handler  = entry_handler,
+    .data_size      = sizeof(struct instance_data),
+    .maxactive      = 20,
 };
 
 static int __init kretprobe_init(void)
 {
-	int ret;
+    int ret;
 
-	my_kretprobe.kp.symbol_name = func_name;
-	ret = register_kretprobe(&my_kretprobe);
-	if (ret < 0) {
-		pr_warn("register_kretprobe failed, returned %d\n", ret);
-		return ret;
-	}
-	return 0;
+    my_kretprobe.kp.symbol_name = func_name;
+    ret = register_kretprobe(&my_kretprobe);
+    if (ret < 0) {
+        pr_warn("register_kretprobe failed, returned %d\n", ret);
+        return ret;
+    }
+    return 0;
 }
 
 static void __exit kretprobe_exit(void)
 {
-	unregister_kretprobe(&my_kretprobe);
-	pr_info("kretprobe at unregistered\n");
-
-	/* nmissed > 0 suggests that maxactive was set too low. */
-	if (my_kretprobe.nmissed) pr_warn("Missed probing %d instances.\n", my_kretprobe.nmissed);
+    unregister_kretprobe(&my_kretprobe);
 }
 
 module_init(kretprobe_init)
